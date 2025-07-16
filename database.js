94f79604017bffd0cd13113ee36899b3b2151c1a
Subhashtechfirst <warkadsubhash9@gmail.com>
2025-07-16 15:50:47 +0530

    JS

---
+++ database.js
@@ -0,0 +1,39 @@
+//  String Variable Example
+ var My_Name= "Subhash";
+ var my_sur_name="Warkad";
+ var my_villege_name="Shewga";
+ var my_taluka_name="Partur";
+ var myPostName="Anandwadi";
+ var my_district_name="Jalna";
+ var MyStateName="Maharashtra";
+ var MyCollegeName="L.B.S.Sr.College Partur";
+ var MyCurrentCity="Pune";
+
+// Number Variable Example
+ let one= 1;
+ let two= 2;
+ let three= 3;
+ let four= 4;
+ let five= 5;
+ let six=6;
+ let seven=7;
+ let eight=8;
+ let nine=9;
+ let ten=10;
+
+ // Array Example
+var fruits= ["Apple", "Banana", "Orange"];
+var colors= ["Red", "Green", "Blue"];
+var number= [10,20,30];
+var day= ["sunday", "monday", "tuesday"];
+var month= ["jan", "mar", "feb"];
+var state= ["maharashtra", "gujrat", "rajsthan"];
+var dist=["jalna","pune", "nanden"];
+var taluka=["partur", "badnapur", "ghansawangi"];
+var villege=[ "shevga", "partur","thane"];
+var name=[ "ram","sham","ghansham"];
+
+// Object Example
+
+
+
